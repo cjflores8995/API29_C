8f00f194826c87b2ff8682c60fbca6f5a91326d3
Carlos Flores <cjflores@29deoctubre.fin.ec>
2019-06-10 14:19:30 -0500

    Dentro del branch libelula se implementa la interfaz y la clase WebEstructurasConciliacion al proyecto

---
+++ Business/Logic/WebEstructurasConciliacion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    class WebEstructurasConciliacion : IWebEstructurasConciliacion
+    {
+        public string GeneraEstructuraConciliacion()
+        {
+            return "estructura generada correctamente";
+        }
+    }
+}
